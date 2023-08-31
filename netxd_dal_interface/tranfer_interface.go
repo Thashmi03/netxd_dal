1900897884dcc2204048eb7b1363cda3c0deca8c
Thashmi03 <thashmigaa.cs20@bitsathy.ac.in>
2023-08-31 16:22:36 +0530

    c

--- netxd_dal_interface/tranfer_interface.go
+++ netxd_dal_interface/tranfer_interface.go
@@ -2,5 +2,5 @@ package netxddalinterface
 
 import tmodel "github.com/Thashmi03/transfer_model"
 type Itransact interface{
-	Transfer(detail * tmodel.Transaction)(error)
+	Transfer(detail * tmodel.Transaction)(string,error)
 }
--- netxd_dal_services/tranfer_services.go
+++ netxd_dal_services/tranfer_services.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-	tmodel "github.com/Thashmi03/transfer_model"
+	model "github.com/Thashmi03/transfer_model"
 	tinterface "github.com/Thashmi03/transfer_interface"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,7 +23,7 @@ func InitTransaction (ccollection *mongo.Collection,tcollection *mongo.Collectio
 	return &Transaction{ctx,client,tcollection,ccollection}
 }
 
-func (t*Transaction)Transfer(detail * tmodel.Transaction)(*tmodel.,error){
+func (t*Transaction)Transfer(detail *model.Transaction)(string,error){
 	session,err:=t.client.StartSession()
 	if err!=nil{
 		log.Fatal(err)
@@ -31,29 +31,29 @@ func (t*Transaction)Transfer(detail * tmodel.Transaction)(*tmodel.,error){
 	defer session.EndSession(context.Background())
 	_,err=session.WithTransaction(context.Background(),func(ctx mongo.SessionContext) (interface{}, error){
 		_, err := t.Customercollection.UpdateOne(ctx,
-			bson.M{"customer_id": from},
-			bson.M{"$inc": bson.M{"balance": -amount}})
+			bson.M{"customer_id": detail.From_account},
+			bson.M{"$inc": bson.M{"balance": -detail.Amount}})
 		if err!=nil{
 			fmt.Println("failed1")
 		}
 		_,err2:=t.Customercollection.UpdateOne(context.Background(),
-		bson.M{"customer_id":to},
-		bson.M{"$inc":bson.M{"balance":amount}},)
+		bson.M{"customer_id":detail.To_account},
+		bson.M{"$inc":bson.M{"balance":detail.Amount}},)
 		if err2!=nil{
 			fmt.Println("failed2")
 		}
-		trans:=&tmodel.Transaction{
+		trans:=&model.Transaction{
 			Transaction_id:"T001",
-			From_account:from,
-			To_account :to,
-			Amount :amount,
+			From_account:detail.From_account,
+			To_account :detail.To_account,
+			Amount :detail.Amount,
 		}
 		res,err:=t.mongoCollection.InsertOne(context.Background(),&trans)
 		if err!=nil{
 			return "nil",err
 		}
 		
-		var newUser *tmodel.Transaction
+	var newUser *model.TResponse
 	query := bson.M{"_id": res.InsertedID}
 	
 	err3 := t.mongoCollection.FindOne(t.ctx, query).Decode(&newUser)
